819e4a09f3fc5877023e2906ba60ab23cfc1444d
wagnerwg0202 <97996373+wagnerwg0202@users.noreply.github.com>
2022-10-07 12:03:44 -0300

    Cronometro React

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from 'react';
+import './style.css'
+//cronometro*******************
+class App extends Component{
+  constructor(props){
+    super(props);
+    this.state={
+        numero: 0,
+        vai : 'Iniciar'
+    }
+    this.timer = null
+    this.vai = this.vai.bind(this)
+    this.limpar = this.limpar.bind(this)
+  }
+  vai(){
+    let state = this.state
+    if(this.timer !== null){ 
+      clearInterval(this.timer);
+      this.timer = null // transformado para nul para cair no else
+      state.vai = 'Iniciar'
+    }else{
+      this.timer = setInterval( () => {
+        state.numero += 0.01
+        this.setState(state)
+      }, 100)
+        state.vai='Pausar'
+    }
+    this.setState(state)//atualizar as states
+  }
+  limpar(){
+    if(this.timer == null){ // == null signigica quando o relogio estiver parado
+      let state = this.state
+      state.numero=0
+      this.setState(state)
+    }else{
+      let state = this.state
+      clearInterval(this.timer) //parar o tempo
+      this.timer = null //transformar valor em null, para cair no else la em cima
+      state.numero=0
+      state.vai='Iniciar'
+      this.setState(state)
+    }
+  }
+  render(){
+    return(
+      <div className='container'>
+        <img className='image' src={require('./assets/cronometro.png')}/>
+        <a className='time'>{this.state.numero.toFixed(1)}</a>
+        <div className='areabtn'>
+          <a className='button' onClick={this.vai}>{this.state.vai}</a>
+          <a className='button' onClick={this.limpar}>Zerar</a>
+        </div>
+      </div>
+    ) }
 }
 
-export default App;
+export default App
